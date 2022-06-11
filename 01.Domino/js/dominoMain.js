dd40bad65668d84ede7270cd37c4c14dbf6563b7
galadriel91 <g_ala@Naver.com>
2022-06-11 16:25:50 +0900

    Simplify: Commonization of JS Files For Main

--- 01.Domino/js/dominoMain.js
+++ 01.Domino/js/dominoMain.js
@@ -1,6 +1,18 @@
-const mainSectionProduct = new Vue({
-    el:'#mainSectionProduct',
+const main = new Vue({
+    el:'#main',
     data:{
+        // MainEvent
+        eventBannerArray:[
+            {src:"./images/main_card1.png" , alt:"회원가입 즉시 20% 할인"},
+            {src:"./images/main_card2.gif" , alt:"도미노 매니아만의 혜택보기"},
+            {src:"./images/main_card3.png" , alt:"희망나눔 캠페인 20%할인"},
+            {src:"./images/main_card4.png" , alt:"나만의 피자 먹고싶은 그날 !"}
+        ],
+        state: 'currentLeft',
+        disable:true,
+
+        // MainProducts
+
         bestUpperArray:[
             {src:"./images/potato.jpg" , alt:"포테이토 피자" , title:"포테이토" , price1:"27,900원" , price2:"21,000원", order:"바로주문" , number:1 , rank:'best' , top:true},
             {src:"./images/bulgogi.jpg" , alt:"불고기 피자" , title:"리얼 불고기" , price1:"27,900원" , price2:"21,000원", order:"바로주문" , number:2 , rank:'normal' , },
@@ -25,6 +37,19 @@ const mainSectionProduct = new Vue({
         state:'present'
     },
     methods:{
+        // MainEvent
+        onClickLeft(){
+            if(this.state != 'currentLeft'){
+                this.state = 'currentLeft'
+                this.disable = true
+            }
+        },
+        onClickRight(){
+            this.state = "moveRight"
+            this.disable = false
+        },
+
+        // MainProducts
         onClickToggle(e){
             if(e.target.className !== "on"){
                 this.toggle = !this.toggle
