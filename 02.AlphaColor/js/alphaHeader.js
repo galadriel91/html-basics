cc02724c475d2cf409c9eab98711420895db8594
galadriel91 <g_ala@Naver.com>
2022-06-28 16:00:28 +0900

    Update: Define Header Drop Down Detail Style

--- 02.AlphaColor/js/alphaHeader.js
+++ 02.AlphaColor/js/alphaHeader.js
@@ -1,4 +1,16 @@
 (()=>{
+    const NavList = document.querySelectorAll('.nav-list')
+    const On = 'on'
+    
+    NavList.forEach((v)=>{
+        v.addEventListener('click' , ()=>{
+            let classTarget = null
+            NavList.forEach(v=>v.classList.remove(On))
+            classTarget = v
+            classTarget.classList.add(On)
+        })
+    })
+
     const responsiveButton = document.querySelector('.responsiveButton')
     const headerBottom = document.querySelector('.headerWrapBottom')
     const responsiveButtonClick = () => {
