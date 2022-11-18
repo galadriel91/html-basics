723959418e5207222bf758a4ce3cf2aea70e6612
GalaDriel <g_ala@naver.com>
2022-11-18 21:19:39 +0900

    Update: Define MainFade.js

---
+++ 05.MainPage/js/mainFade.js
@@ -0,0 +1,12 @@
+(()=>{
+    const fadeTargets = document.querySelectorAll('.fateTarget')
+    const FADEON = "fadeOn"
+    const heightCheck = () => {
+        for(let i = 0; i <fadeTargets.length ; i++){
+            if(screenY > fadeTargets[i].getBoundingClientRect().top * .9){
+                fadeTargets[i].classList.add(FADEON)
+            }
+        }
+    }
+    document.addEventListener('scroll' , heightCheck)
+})()
