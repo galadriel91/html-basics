445c184c25969bd7baffdf63258653c3d9436b3b
GalaDriel <g_ala@naver.com>
2023-02-15 14:31:21 +0900

    Update: Apply event & products slide

---
+++ 01.Airbusan/js/mainEvent.js
@@ -0,0 +1,9 @@
+$('.mainEventSlide').slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows:true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    accessibility:false,
+    pauseOnDotsHover:true,
+});
