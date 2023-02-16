9b7a6e3317339f1f496ed66fa9f59394dd282173
GalaDriel <g_ala@naver.com>
2023-02-16 17:54:33 +0900

    Update: Define MD & Menu Slide

---
+++ 04.Hollys/js/hollysMenu.js
@@ -0,0 +1,9 @@
+$('.menuSlide').slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows:false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    accessibility:false,
+    pauseOnDotsHover:true,
+});
