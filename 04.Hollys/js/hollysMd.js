aa940dc9885d4d7cc0ae9884e833f5e21a2cfb58
GalaDriel <g_ala@naver.com>
2023-02-16 23:12:45 +0900

    Update: Define Hollys Coffee

--- 04.Hollys/js/hollysMd.js
+++ 04.Hollys/js/hollysMd.js
@@ -6,4 +6,33 @@ $('.mdSlide').slick({
     slidesToScroll: 1,
     accessibility:false,
     pauseOnDotsHover:true,
+    responsive: [
+        {
+            breakpoint: 1150,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 830,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 401,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+    ]
 });
