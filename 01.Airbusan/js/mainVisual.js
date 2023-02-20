4b8dd3001a742c63e14d5e9126ecbb6f3110f81d
GalaDriel <g_ala@naver.com>
2023-02-20 11:41:18 +0900

    Update: Apply Visual Dots logic

--- 01.Airbusan/js/mainVisual.js
+++ 01.Airbusan/js/mainVisual.js
@@ -6,3 +6,18 @@ $('.mainSlide').slick({
     accessibility:false,
     pauseOnDotsHover:true
 });
+
+const dots = document.querySelectorAll('.slick-dots button')
+
+dots.forEach((item)=>{
+    item.addEventListener('click' , ()=>{
+        $('.mainSlide').slick('slickPause');
+        setTimeout(()=>{
+            $('.mainSlide').slick('slickPlay');
+        },1000)
+    })
+})
+
+$('.mainSlide').on('touchstart', e => {
+    $('.mainSlide').slick('slickPlay');
+});
