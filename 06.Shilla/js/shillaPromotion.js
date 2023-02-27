0d9e92c394dd843836602a16a7b42db733e631a0
GalaDriel <g_ala@naver.com>
2023-02-27 13:56:47 +0900

    Add: ShilaPromotion.js

---
+++ 06.Shilla/js/shillaPromotion.js
@@ -0,0 +1,9 @@
+$('.promotionSlide').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows:false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    accessibility:false,
+    pauseOnDotsHover:true,
+});
