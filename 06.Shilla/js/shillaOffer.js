c0efdf9e3a07713f29d0c4c184d7dac68e0f7bb2
GalaDriel <g_ala@naver.com>
2023-03-04 21:56:01 +0900

    Update: Apply shillaOffer responsive

---
+++ 06.Shilla/js/shillaOffer.js
@@ -0,0 +1,29 @@
+$('.mobileSlide').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows:false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    accessibility:false,
+    pauseOnDotsHover:true,
+    responsive: [
+        {
+            breakpoint: 970,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 550,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+    ]
+});
