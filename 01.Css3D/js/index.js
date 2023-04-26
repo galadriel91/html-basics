0de894a946d81210b4a129f5a89ee1f5bf0a3180
GalaDriel <g_ala@naver.com>
2023-04-26 18:21:36 +0900

    Update: Simple function defined

--- 01.Css3D/js/index.js
+++ 01.Css3D/js/index.js
@@ -1,9 +1,21 @@
 const containerElem = document.querySelector('.container')
-const leftClass = 'left'
-const rightClass = 'right'
+
 
 const handleClick = (e) => {
-    console.log(e.target.parentNode)
+    const targetElem = e.target
+    if(targetElem.parentNode.classList.contains('firstCard')){
+        targetElem.parentNode.classList.add('open')
+    }
+    if(targetElem.parentNode.classList.contains('lastCard')){
+        targetElem.parentNode.classList.add('open')
+    }
+    if(targetElem.classList.contains('closeBtn')){
+        const openElem = document.querySelectorAll('.open')
+        openElem[1].classList.remove('open')
+        setTimeout(()=>{
+            openElem[0].classList.remove('open')
+        },500)
+    }
 }
 
 containerElem.addEventListener('click', handleClick)
