33494a99b3c07cb57d94fdbb692cf5a2fe74568d
GalaDriel <g_ala@naver.com>
2023-04-27 20:34:14 +0900

    Update: Define stage translateZ function

--- 05.3DSpace/js/index.js
+++ 05.3DSpace/js/index.js
@@ -1,6 +1,8 @@
 (()=>{
     const progress = document.querySelector('.progress')
+    const stage = document.querySelector('.stage')
     const house = document.querySelector('.house')
+    const mousePos = { x: 0, y: 0 };
     let maxScrollValue;
 
     const handleResize = () => {
@@ -13,7 +15,14 @@
         house.style.transform = `translateZ(${scrollPer * 980 - 490}vw)`
     }
 
+    const handleMousemove = (e) => {
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2
+        stage.style.transform = `rotateX(${mousePos.y * 5}deg) rotateY(${mousePos.x * 5}deg)`
+    }
+
     window.addEventListener('scroll' , handleProgress)
     window.addEventListener('resize' , handleResize)
+    window.addEventListener('mousemove', handleMousemove)
     handleResize()
 })()
