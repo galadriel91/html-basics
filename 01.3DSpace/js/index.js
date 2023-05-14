57db6acea1606fd35196c29ba7f70048a980b08b
GalaDriel <g_ala@naver.com>
2023-05-14 22:43:48 +0900

    Update: Define handleMousemove

--- 01.3DSpace/js/index.js
+++ 01.3DSpace/js/index.js
@@ -1,17 +1,29 @@
 (()=>{
     const wallWrap = document.querySelector('.spaceWallWrap')
+    const spaceWrap = document.querySelector('.spaceWrap')
+    const mousePos={
+        x:0,
+        y:0
+    }
     let maxScrollValue;
 
     const checkScrollValue = () => {
         maxScrollValue = document.body.offsetHeight - window.innerHeight
     }
-    
 
     const handleScroll = () => {
         const scrollPer = window.scrollY / maxScrollValue
         wallWrap.style.transform = `translateZ(${scrollPer * 980 - 490}vw)`
     }
 
+    const handleMousemove = (e) => { 
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2
+        spaceWrap.style.transform = `rotateX(${mousePos.y * 5}deg) rotateY(${mousePos.x * 5}deg)`
+    }   
+
     checkScrollValue()
-    window.addEventListener('scroll' , handleScroll)
+    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('mousemove', handleMousemove)
+    window.addEventListener('resize', checkScrollValue)
 })();
