6db07226768fff0c36dfb773cb2fc162695208ce
GalaDriel <g_ala@naver.com>
2023-05-15 14:27:24 +0900

    Update: Define handleMousemove

--- 03.IntroPage/js/index.js
+++ 03.IntroPage/js/index.js
@@ -1,5 +1,10 @@
 (()=>{
     const wallWrap = document.querySelector('.introWallWrap')
+    const world = document.querySelector('.introWorld')
+    const mousePos = {
+        x:0,
+        y:0
+    }
     let maxScrollValue
 
     const checkMaxScrollValue = () => {
@@ -11,7 +16,14 @@
         wallWrap.style.transform = `translateZ(${scrollPer * 980 - 490}vw)`
     }
 
+    const handleMousemove = (e) => {
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2
+        world.style.transform = `rotateX(${mousePos.y * 5}deg) rotateY(${mousePos.x * 5}deg)`
+    }
+
     checkMaxScrollValue()
     window.addEventListener('resize' , checkMaxScrollValue)
     window.addEventListener('scroll' , handleScroll)
+    window.addEventListener('mousemove' , handleMousemove)
 })()
